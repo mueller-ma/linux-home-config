0b91915879e40a330cf849ad9d7eee176ea892ac
mueller-ma <mueller-ma@users.noreply.github.com>
2016-11-03 09:19:00 +0100

    changed README

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Config files in linux home dir
 
 This repo is currently unstable!
+
+## Dependences
+* bash
+* tar
+* wget or curl
--- install.sh
+++ install.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
+home_path="~/.linux-home-config"
+
 function wgetOrCurl {
 	if hash curl 2>/dev/null
 	then
-		WGET_OR_CURL=curl
+		WGET_OR_CURL="curl"
+		WGET_OR_CURL_CMD="curl -s -L"
 	elif hash wget 2>/dev/null
 	then
-		WGET_OR_CURL=wget
+		WGET_OR_CURL="wget"
+		WGET_OR_CURL_CMD="wget --quiet"
 	else
-		echo "wget or curl need." && exit 1
+		echo "wget or curl needed"
+		exit 1
 	fi
 }
 
 function setAutoSync {
+	mkdir ${home_path}/download
+	cd ${home_path}/download
+	
 }
 
 
@@ -22,20 +30,22 @@ function localMirrorQ {
 	select yn in "Yes" "No" "Cancel"; do
 	    case $yn in
 		Yes ) localMirrorURLQ; break;;
-		No ) URL="https://github.com/mueller-ma/linux-home-config"; break;;
+		No ) URL="https://github.com/mueller-ma/linux-home-config/archive/master.tar.gz"; break;;
 		Cancel ) exit;;
 	    esac
 	done
 }
 
 function localMirrorURLQ {
-	echo "Enter the URL: "
-	read URL
+	echo "Enter the URL (\"/archive/master.tar.gz\" will be added): "
+	read input_var
+	URL=${input_var}/archive/master.tar.gz
+	unset input_var
 }
 
 #cd ~
-#mkdir -p .linux-home-config/old-config-files
-#mv -t .linux-home-config/old-config-files .vimrc .vim .bashrc .bash_aliases
+#mkdir -p ${home_path}/old-config-files
+#mv -t ${home_path}/old-config-files .vimrc .vim .bashrc .bash_aliases
 
 wgetOrCurl
 
