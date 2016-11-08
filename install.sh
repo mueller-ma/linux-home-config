74c6c06cbf9a115f56b4fc872f4be14df32097cb
mueller-ma <mueller-ma@users.noreply.github.com>
2016-11-08 16:05:27 +0100

    generate help

--- install.sh
+++ install.sh
@@ -61,6 +61,7 @@ function downloadAll {
 	cp update.sh ${HOME}/${home_path}/update.sh
 	# remove tar ball
 	rm ../master.tar.gz
+	vi -c ':Helptags' -c ':q' > /dev/null
 }
 
 function downloadGitmodules {
