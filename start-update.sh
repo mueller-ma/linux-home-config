eff398667b38363647db6cea1c1b66bf36b6ef35
mueller-ma <mueller-ma@users.noreply.github.com>
2017-02-07 13:14:03 +0100

    update.sh not executable

--- start-update.sh
+++ start-update.sh
@@ -6,5 +6,6 @@ tempfile="${HOME}/${home_path}/temp"
 configfile="${HOME}/${home_path}/config"
 
 mv "${HOME}/${home_path}/update.sh" "${HOME}/${home_path}/update.old.sh"
+chmod +x "${HOME}/${home_path}/update.old.sh"
 "${HOME}/${home_path}/update.old.sh"
 rm "${HOME}/${home_path}/update.old.sh"
